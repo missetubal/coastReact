697cb1857cabd2dfdd4f557f263705b6e33a207d
Michelle Setúbal <missetubal@outlook.com>
2022-06-05 15:02:56 -0300

    feat: navbar component

---
+++ src/components/Navbar/navbar.components.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+import { Nav, NavOptions, Pages } from "./navbar.style"
+// import logo from '../../assets/'
+
+export default function Navbar() {
+    return (
+        <Nav>
+            <Pages href='./'>
+                <img src={'../../assets/logo.png'} alt='logo' />
+            </Pages>
+            <NavOptions>
+                <Pages href="/">Home</Pages>
+                <Pages href='./prjects'>Projects</Pages>
+                <Pages href='./company'>Company</Pages>
+                <Pages href='./contact'>Contact</Pages>
+            </NavOptions>
+
+        </Nav>
+    );
+};
---
+++ src/components/Navbar/navbar.style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+
+export const Nav = styled.nav`
+    display: flex;
+    background-color: #222;
+    justify-content: space-between;
+    padding: 1em;
+    align-items: center;
+    color: white;
+`
+export const NavOptions = styled.ul`
+    list-style: none;
+    padding: 5px;
+    margin-right: 10px;
+`
+export const Pages = styled.a`
+    color: white;
+    margin: 10px;
+    text-decoration: none;
+    text-transform: uppercase;
+    :hover{
+        color: gray;
+    }
+`
